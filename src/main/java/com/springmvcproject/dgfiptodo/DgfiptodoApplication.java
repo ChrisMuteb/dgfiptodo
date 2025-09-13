cbe4f6a21f9d063eac12fb6f7eec892300971091
chriskayomb <ibond.chris@gmail.com>
2025-09-13 10:55:55 +0200

    Initial commit

---
+++ src/main/java/com/springmvcproject/dgfiptodo/DgfiptodoApplication.java
@@ -0,0 +1,13 @@
+package com.springmvcproject.dgfiptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgfiptodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgfiptodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvcproject/dgfiptodo/DgfiptodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvcproject.dgfiptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgfiptodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
